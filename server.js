c5f0159b3ddf9406e0f33e064ca7182ffe447e91
Den Pol <selection4test@gmail.com>
2020-08-06 22:55:36 +0300

    ssl. fix

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
-app.use(cors());
-const server = require("http").Server(app);
+// const app = express();
+// const cors = require("cors");
+// app.use(cors());
+// const server = require("http").Server(app);
 // const io = require("socket.io")(server);
+var app = express.createServer(options);
 const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
+const peerServer = ExpressPeerServer(app, {
   debug: true,
 });
 const { v4: uuidV4 } = require("uuid");
